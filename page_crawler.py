4c7e20d8a719d7e6020c01ccf51833e49839e180
Martin Hwang <dhhwang89@gmail.com>
2020-06-05 03:16:28 +0900

    fix: collect id, page crawler
     page id 수집기에서 계속 같은 page 사이를 멤도는 로직 수정

--- page_crawler.py
+++ page_crawler.py
@@ -1,15 +1,20 @@
+import os
 import json
 from crawler import (PageIDCollector, PageCrawler)
 
 if __name__ == "__main__":
     directory = "json"
-    page_id_collector = PageIDCollector()
-    page_id_list = page_id_collector.page_id_list
+    url = "https://www.myhome.go.kr/hws/portal/sch/selectRsdtRcritNtcDetailView.do?pblancId="
+
+    with open("page_ids.json", "r") as f:
+        page_id_dict = json.load(f)
+
+    page_id_list = page_id_dict["ids"]
 
     for page_id in page_id_list:
         url = PageIDCollector.url + page_id
         page_crawler = PageCrawler(url)
         crawled_info_with_json = page_crawler.crawled_info_with_json
 
-        with open(directory + "page_id" + ".json", "w") as f:
+        with open(os.path.join(directory, (str(page_id) + ".json")), "w") as f:
             json.dump(crawled_info_with_json, f)
